c15735bfca6d5812e81ce5bc77a8c7dee636ec87
BLACKSQUID24 <beergram.40@gmail.com>
2026-02-26 15:30:30 +0700

    APP

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'asthma-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+// ติดตั้งและเก็บ Cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// เรียกใช้ไฟล์จาก Cache ถ้าไม่มีเน็ต
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
